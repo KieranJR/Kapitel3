46811f7d99f05b93793b31d5489c58b357909edf
Kieran Rye <ryki16@nti.umea>
2017-09-25 10:25:22 +0200

    first commit

---
+++ numbers.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+/* Vad? visar olika värder
+	Vem av? Kieran
+	När? 25:e September 2017
+*/
+
+public class numbers {
+	public static void main (String[] arg) {
+		int l;
+		double x;
+		l = 8900;
+		x = 5;
+
+
+	}
+
+}
